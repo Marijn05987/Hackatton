acc4231c9669486d9e5aae1b9097023576383741
Marijn05987 <Marijnjulian@gmail.com>
2025-03-26 14:25:09 +0100

    Update hackatontest.py

--- hackatontest.py
+++ hackatontest.py
@@ -8,7 +8,7 @@ import seaborn as sns
 # Cache de gegevensophaal functie om onnodige herhalingen van verzoeken te voorkomen
 @st.cache_data
 def fetch_data():
-    url = 'https://sensornet.nl/dataserver3/event/collection/nina_events/stream?conditions%5B0%5D%5B%5D=time&conditions%5B0%5D%5B%5D=%3E%3D&conditions%5B0%5D%5B%5D=1735689600&conditions%5B1%5D%5B%5D=time&conditions%5B1%5D%5B%5D=%3C&conditions%5B1%5D%5B%5D=1742774400&conditions%5B2%5D%5B%5D=label&conditions%5B2%5D%5B%5D=in&conditions%5B2%5D%5B%5D=21&conditions%5B2%5D%5B%5D=32&conditions%5B2%5D%5B%5D=33&conditions%5B2%5D%5B%5D=34&args%5B%5D=aalsmeer&args%5B%5D=schiphol&fields%5B%5D=time&fields%5B%5D=location_short&fields%5B%5D=location_long&fields%5B%5D=duration&fields%5B%5D=SEL&fields%5B%5D=SELd&fields%5B%5D=SELe&fields%5B%5D=SELn&fields%5B%5D=SELden&fields%5B%5D=SEL_dB&fields%5B%5D=lasmax_dB&fields%5B%5D=callsign&fields%5B%5D=type&fields%5B%5D=altitude&fields%5B%5D=distance&fields%5B%5D=winddirection&fields%5B%5D=windspeed&fields%5B%5D=label&fields%5B%5D=hex_s&fields%5B%5D=registration&fields%5B%5D=icao_type&fields%5B%5D=serial&fields%5B%5D=operator&fields%5B%5D=tags'
+    url = 'https://sensornet.nl/dataserver3/event/collection/nina_events/stream?conditions%5B0%5D%5B%5D=time&conditions%5B0%5D%5B%5D=%3E%3D&conditions%5B0%5D%5B%5D=1735689600&conditions%5B1%5D%5B%5D=time&conditions%5B1%5D%5B%5D=%3C&conditions%5B1%5D%5B%5D=1742774400&conditions%5B%5D%5B%5D=label&conditions%5B%5D%5B%5D=in&conditions%5B%5D%5B%5D=21&conditions%5B%5D%5B%5D=32&conditions%5B%5D%5B%5D=33&conditions%5B%5D%5B%5D=34&args%5B%5D=aalsmeer&args%5B%5D=schiphol&fields%5B%5D=time&fields%5B%5D=location_short&fields%5B%5D=location_long&fields%5B%5D=duration&fields%5B%5D=SEL&fields%5B%5D=SELd&fields%5B%5D=SELe&fields%5B%5D=SELn&fields%5B%5D=SELden&fields%5B%5D=SEL_dB&fields%5B%5D=lasmax_dB&fields%5B%5D=callsign&fields%5B%5D=type&fields%5B%5D=altitude&fields%5B%5D=distance&fields%5B%5D=winddirection&fields%5B%5D=windspeed&fields%5B%5D=label&fields%5B%5D=hex_s&fields%5B%5D=registration&fields%5B%5D=icao_type&fields%5B%5D=serial&fields%5B%5D=operator&fields%5B%5D=tags'
     
     try:
         response = requests.get(url)
@@ -55,6 +55,7 @@ def bereken_geluid_per_passagier_en_vracht(data, vliegtuig_capaciteit, load_fact
             
             results.append({
                 'vliegtuig_type': vliegtuig_type,
+                'passagiers': passagiers,
                 'geluid_per_passagier': geluid_per_passagier,
                 'geluid_per_vracht': geluid_per_vracht
             })
@@ -119,3 +120,32 @@ plt.tight_layout()
 
 # Toon de grafiek in Streamlit
 st.pyplot(fig)
+
+# Groeperen op passagiers aantal en vergelijken
+st.subheader('Vergelijking van Vliegtuigen op Basis van Passagiersaantal')
+
+# Categoriseer vliegtuigen op basis van passagiers
+def categorize_by_passenger(passenger_count):
+    if passenger_count <= 100:
+        return '0-100 Passagiers'
+    elif passenger_count <= 150:
+        return '101-150 Passagiers'
+    elif passenger_count <= 200:
+        return '151-200 Passagiers'
+    else:
+        return '201+ Passagiers'
+
+resultaten['passagiers_categorie'] = resultaten['passagiers'].apply(categorize_by_passenger)
+
+# Maak de grafiek voor de categorisatie
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='passagiers_categorie', y='geluid_per_passagier', data=resultaten, palette='Set2')
+
+plt.title('Vergelijking van Geluid per Passagier per Passagierscategorie', fontsize=16)
+plt.xlabel('Passagierscategorie', fontsize=12)
+plt.ylabel('Geluid per Passagier (dB)', fontsize=12)
+plt.xticks(rotation=45)
+
+# Toon de grafiek in Streamlit
+st.pyplot(plt)
+
